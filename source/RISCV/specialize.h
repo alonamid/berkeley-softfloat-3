e68e3255e82634c8a03e210b3dc6b72d61ef715c
Ryan Lund <ryan.lund@berkeley.edu>
2020-04-19 09:53:09 -0700

    Fix naming in specalize.h

--- source/RISCV/specialize.h
+++ source/RISCV/specialize.h
@@ -142,8 +142,8 @@ uint_fast16_t
 | the combined NaN result.  If either 'uiA' or 'uiB' has the pattern of a
 | signaling NaN, the invalid exception is raised.
 *----------------------------------------------------------------------------*/
-uint_fast32_t
- softfloat_propagateNaNBF32UI( uint_fast16_t uiA, uint_fast16_t uiB );
+uint_fast16_t
+ softfloat_propagateNaNBF16UI( uint_fast16_t uiA, uint_fast16_t uiB );
 
 /*----------------------------------------------------------------------------
 | The bit pattern for a default generated 32-bit floating-point NaN.
